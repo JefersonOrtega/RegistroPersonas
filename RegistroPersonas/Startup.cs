a5d1b03108003db6dd6d6e95982db45ca285d441
Jeferson Raul Ortega Brito <jeferson_ortega@ucne.edu.do>
2020-05-23 14:26:30 -0300

    Completado

--- RegistroPersonas/Startup.cs
+++ RegistroPersonas/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RegistroPersonas.Data;
+using Blazored.Toast;
 
 namespace RegistroPersonas
 {
