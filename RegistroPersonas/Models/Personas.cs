fe7e011eee0e421550b5ff077df91f845b649147
Jeferson Raul Ortega Brito <jeferson_ortega@ucne.edu.do>
2020-05-22 21:31:58 -0300

    BLL Terminada

---
+++ RegistroPersonas/Models/Personas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPersonas.Models
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el nombre")]
+        public string Nombre { get; set; }
+
+        [Phone(ErrorMessage = "Debe introducir un número de teléfono válido")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "Es obigatorio introducir su cédula")]
+        public string Cedula { get; set; }
+
+        [Required(ErrorMessage ="Es obligatorio introducir su dirección")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir su fecha de nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+
+    }
+}
